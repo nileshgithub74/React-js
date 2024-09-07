a4ea788b62d713accaee161c5f1ea8d89d9808ca
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-09-07 19:35:21 +0000

    react

---
+++ react-app/src/Footer.jsx
@@ -0,0 +1,17 @@
+
+
+
+function Footer(){
+    return(
+      
+
+    <footer>
+        <p>@Copy... React's Website</p>
+    </footer>
+
+
+    );
+}
+
+
+export default Footer
---
+++ react-app/src/Header.jsx
@@ -0,0 +1,24 @@
+
+
+
+function  Header(){
+    return(
+        <header>
+            <h1>This is my first React Website</h1>
+
+            <nav>
+               <ul>
+                <li> <a href="#">Home</a></li>
+                <li><a href="#">ABout</a></li>
+                <li><a href="#">Contact us</a></li>
+                <li><a href="#">help</a></li>
+
+               </ul>
+            </nav>
+
+        </header>
+    );
+
+}
+
+export default Header
