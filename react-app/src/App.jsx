7abe70e3cc17f1af0c42209945327ad31a93ddc9
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-09-07 20:41:59 +0000

    react js

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,19 +1,20 @@
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
-import Button from './Button/Button.jsx';
+// import Header from './Header.jsx';
+// import Footer from './Footer.jsx';
+// import Button from './Button/Button.jsx';
+// import Cart from './Cart.jsx'
 
-import Cart from './Cart.jsx'
+import Student from "./Student.jsx"
 function App() {
   
 
   return (
     <>
-    <Header/>
-    <Footer></Footer>
-    <Cart/>
-   
-    <Button/>
     
+    <Student name="Nilesh Kumar"  age={ 30}  isStudent={false}/>
+    <Student name="sreyansh"  age={ 30}  isStudent={true}/>
+    <Student name="sonu"  age={ 30}  isStudent={false}/>
+    <Student name="Nileshsreyansh"  age={ 30}  isStudent={true}/>
+    <Student name="komal"  age={ 30}  isStudent={false}/>
     </>
   )
 }
